487ef502f0a0df0f8fce0e2b30b066ca83a4dd3c
TKUIL <153629020+TKUIL@users.noreply.github.com>
2024-11-01 19:29:47 +0200

    pathes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use(middleware.unknownEndpoint)
