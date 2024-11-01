f976bf2b983b1fb3f46b5ff23ef52a125e38bf8d
TKUIL <153629020+TKUIL@users.noreply.github.com>
2024-11-01 19:23:30 +0200

    path

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const path = require('path')
 
 mongoose.set('strictQuery', false)
 
